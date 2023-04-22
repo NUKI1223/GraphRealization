7c4d05d207cc0e5efd111d307389cc1bcd92dcfe
Vlad Karpov <mrdelron966@gmail.com>
2023-04-22 16:56:34 +0600

    Добавьте файлы проекта.

---
+++ GraphRealization/Node.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphRealization
+{
+    public class Node
+    {
+        private readonly List<Edge> edges = new List<Edge>();
+        public readonly int NodeNumber;
+        public Node(int number)
+        {
+            NodeNumber = number;
+        }
+        public IEnumerable<Node> IncidentNode
+        {
+            get
+            {
+                return edges.Select(x => x.OtherNode(this));
+            }
+        }
+        public IEnumerable<Edge> IncidentEdge
+        {
+            get
+            {
+                foreach (var e in edges) yield return e;
+                
+            }
+        }
+        public static Edge Connect(Node node1, Node node2, Graph graph)
+        {
+            if (!graph.Nodes.Contains(node1) || !graph.Nodes.Contains(node2)) throw new ArgumentException();
+            var edge = new Edge(node1, node2);
+            node1.edges.Add(edge);
+            node2.edges.Add(edge);
+            return edge;
+        }
+        public static void Disconnect (Edge edge)
+        {
+            edge.From.edges.Remove(edge);
+            edge.To.edges.Remove(edge);
+        }
+    }
+}
